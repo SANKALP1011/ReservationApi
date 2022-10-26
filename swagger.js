951df9f8a644cbb6c3a8cd584f6eb50f6d1289cf
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2022-10-27 00:03:11 +0530

    done

--- swagger.js
+++ swagger.js
@@ -17,13 +17,13 @@ const doc = {
       "This is the authenticated booking api where user can book their flights , cabs and trains and make the payment for it which is protected with the help of stripe.js",
   },
   host: "localhost:3001",
-  schemes: ["http"],
+  schemes: ["https"],
   securityDefinitions: {
     bearerAuth: {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT'
-    }
-}
+      type: "https",
+      scheme: "bearer",
+      bearerFormat: "JWT",
+    },
+  },
 };
 swaggerAutogen(outputFile, endpointsFiles, doc);
