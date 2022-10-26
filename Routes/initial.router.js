eecaf7c40aea1da929ed454f4dd3347d1a27e655
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2022-10-26 23:45:11 +0530

    swagger dpcs done , api v1.1 done

--- Routes/initial.router.js
+++ Routes/initial.router.js
@@ -3,7 +3,7 @@ const InitialRouter = express.Router();
 
 InitialRouter.get("/", (req, res) => {
   res.send(
-    "Welcome to the reservation api , check the github for documentation and postamn link"
+    "Welcome to the reservation api , check the github for documentation and postamn link. For Swagger docs , just put /v1 as the endpoint."
   );
 });
 module.exports = InitialRouter;
